b077894fe9498a496d5d4190f03c8b6e753c9c9f
Ramon Creager <rcreager@nrao.edu>
2016-05-10 16:38:00 -0400

    Added an example broker app

---
+++ examples/test_ppp_broker.py
@@ -0,0 +1,34 @@
+######################################################################
+#  test_ppp_broker.py - Example program to demonstrate the broker.
+#
+#  Copyright (C) 2016 Associated Universities, Inc. Washington DC, USA.
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#  General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+#  Correspondence concerning GBT software should be addressed as follows:
+#  GBT Operations
+#  National Radio Astronomy Observatory
+#  P. O. Box 2
+#  Green Bank, WV 24944-0002 USA
+#
+######################################################################
+
+import logging as log
+from PyProximity import broker
+
+
+if __name__ == '__main__':
+    log.basicConfig(level=log.WARNING)
+    broker('tcp://*:5555', 'tcp://*:5556', 'tcp://*:5557')
